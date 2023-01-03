92a2dc8fcd48ac41e567ba1cfd8b5085f9d4ae7e
Andreas Schörgenhumer <andischoe@gmail.com>
2023-01-03 15:10:58 +0100

    Major refactoring and addition of features (see description for details)
    - refactoring into utils file
    - minor refactoring improvements (like renaming Dataframes into xyz_df for consistency and extending formatting of comments)
    - add functionality to sort submissions given additional meta data

--- splitting/split.py
+++ splitting/split.py
@@ -1,82 +1,20 @@
 import argparse
-import math
 import os.path
-import re
 import shutil
 import zipfile
-from collections.abc import Iterable, Sequence
 from glob import glob
 
 import numpy as np
 import pandas as pd
 
+from util import extract_exercise_number, extract_weighted_tutors, handle_duplicate_names, get_file_path, \
+    get_submissions_df, match_full_names, weighted_chunks
 
-def extract_exercise_number(submissions_file: str, exercise_names: Iterable[str]):
-    for ex_name in exercise_names:
-        match = re.search(rf"{ex_name}[\s\-_]*(\d+)", os.path.basename(submissions_file))
-        if match:
-            return int(match.group(1))
-    raise ValueError("could not automatically infer exercise number, must specify manually via '-n'")
+FULL_NAME_COL = "full_name"
+MOODLE_ID_COL = "moodle_id"
+SUBMISSION_COL = "submission_file"
 
-
-def extract_weighted_tutors(tutors_list: Sequence[str]):
-    # Quick check to determine whether weights are specified.
-    if "," in tutors_list[0]:
-        rows = []
-        for t in tutors_list:
-            if "," not in t:
-                raise ValueError(f"expected ',' in tutor entry '{t}'")
-            name, weight = t.split(",", maxsplit=1)
-            rows.append([name, float(weight)])
-        return pd.DataFrame(rows)
-    for t in tutors_list:
-        if "," in t:
-            raise ValueError(f"unexpected ',' in tutor entry '{t}'")
-    return pd.DataFrame(tutors_list)
-
-
-def handle_duplicate_names(tutors_df: pd.DataFrame):
-    dup = tutors_df["name"].duplicated(keep=False)
-    dup_names = tutors_df["name"][dup]
-    # Create a count for each unique tutor name.
-    counts = dict()
-    
-    def update_and_get_count(name: str):
-        count = counts.get(name, 0)
-        count += 1
-        counts[name] = count
-        return count
-    
-    # Change DataFrame inplace.
-    tutors.loc[dup, "name"] = [f"{dn} ({update_and_get_count(dn)})" for dn in dup_names]
-
-
-def weighted_chunks(s: Sequence, weights: Iterable):
-    # Scale weights to sum = 1.
-    weights = np.array(weights, dtype=float) / sum(weights)
-    chunk_sizes = [math.floor(len(s) * w) for w in weights]
-    # Distribute the remaining elements evenly. Just repeatedly increase each
-    # chunk size by 1 until we distributed all remaining elements.
-    remainder_size = len(s) - sum(chunk_sizes)
-    idx = 0
-    while remainder_size > 0:
-        chunk_sizes[idx] += 1
-        idx = (idx + 1) % len(chunk_sizes)
-        remainder_size -= 1
-    assert sum(chunk_sizes) == len(s)
-    # Chunk sizes are all set, now simply collect each chunk from "s".
-    chunks = []
-    idx = 0
-    for chunk_size in chunk_sizes:
-        chunks.append(s[idx:idx + chunk_size])
-        idx += chunk_size
-    assert sum([len(c) for c in chunks]) == len(s)
-    return chunks
-
-
-def get_file_path(path: str, absolute: bool):
-    return path if absolute else os.path.basename(path)
-
+# TODO: encoding for every read and write access
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-sf", "--submissions_file", type=str, required=True,
@@ -108,43 +46,129 @@ tutors_group.add_argument("-tl", "--tutors_list", type=str, nargs="+",
 parser.add_argument("--print_abs_paths", action="store_true",
                     help="If specified, the printed output will show the absolute paths of all files. Otherwise, only "
                          "the base filenames will be printed (default).")
+parser.add_argument("--sorting_keys", type=str, nargs="+", default=[],
+                    help="If specified, the submissions will be sorted according to these keys. The keys must be part "
+                         "of the header entries in the '--info_file', so this argument must be specified in addition. "
+                         "By default, the submissions will simply be sorted according to their names.")
+parser.add_argument("--submission_renaming_keys", type=str, nargs="+", default=[],
+                    help="If specified, the submissions will be renamed according to these keys. The renaming will be "
+                         "as follows: <key_1><sep><key_2>...<sep><key_n>, where <key_i> is the i-th key of these "
+                         "specified renaming keys and <sep> is the separator as defined by argument "
+                         "'--submission_renaming_separator'. The keys must be part of the header entries in the "
+                         "'--info_file', so this argument must be specified in addition. Note that these keys "
+                         "should lead to unique values for each submission, since otherwise, duplicate submission "
+                         "names would be created, which leads to overwriting and hence loss of data. By default, the "
+                         "original submission names will be used.")
+parser.add_argument("--submission_renaming_separator", type=str, default="_",
+                    help="The separator to use when renaming submissions (see '--submission_renaming_keys'). Has no "
+                         "effect in case '--submission_renaming_keys' is not set. Default: '_'")
+parser.add_argument("--info_file", type=str,
+                    help="If specified, this must be a CSV file containing student (meta-)data that can be used for "
+                         "sorting and renaming submissions. The CSV file must include a header, and these exact header "
+                         "entries can be used as sort/renaming keys as defined by arguments '--sorting_keys' and "
+                         "'--submission_renaming_keys', respectively.")
+parser.add_argument("--info_file_first_name_key", type=str,
+                    help="Only relevant if '--sorting_keys' is specified. If so, this argument indicates the header "
+                         "entry in the '--info_file' which contains the first name. Must be specified together with "
+                         "argument '--info_file_last_name_key'. If not specified, then the first name header entry is "
+                         "tried to be identified automatically based on string matching.")
+parser.add_argument("--info_file_last_name_key", type=str,
+                    help="Only relevant if '--sorting_keys' is specified. If so, this argument indicates the header "
+                         "entry in the '--info_file' which contains the last name. Must be specified together with "
+                         "argument '--info_file_first_name_key'. If not specified, then the last name header entry is "
+                         "tried to be identified automatically based on string matching.")
 args = parser.parse_args()
+
+if args.sorting_keys and args.info_file is None:
+    raise ValueError("must also specify '--info_file' when specifying '--sorting_keys'")
+if args.submission_renaming_keys and args.info_file is None:
+    raise ValueError("must also specify '--info_file' when specifying '--submission_renaming_keys'")
+if args.sorting_keys and bool(args.info_file_first_name_key) != bool(args.info_file_last_name_key):
+    raise ValueError("must specify either both or none of '--info_file_first_name_key' and 'info_file_last_name_key'")
+if args.info_file:
+    info_df = pd.read_csv(args.info_file)
+    
+    
+    def check_keys(keys, arg_name):
+        for k in keys:
+            if k not in info_df.columns:
+                raise ValueError(f"'--{arg_name}' contains '{k}' which is not part of the header entries in "
+                                 f"'--info_file': {info_df.columns.to_list()}")
+    
+    
+    if args.sorting_keys:
+        check_keys(args.sorting_keys, "sorting_keys")
+        if args.info_file_first_name_key:
+            check_keys([args.info_file_first_name_key], "info_file_first_name_key")
+        if args.info_file_last_name_key:
+            check_keys([args.info_file_last_name_key], "info_file_last_name_key")
+    if args.submission_renaming_keys:
+        check_keys(args.submission_renaming_keys, "submission_renaming_keys")
+        renaming_info_df = info_df[args.submission_renaming_keys]
+        duplicates = renaming_info_df[renaming_info_df.duplicated()].drop_duplicates().reset_index(drop=True)
+        if len(duplicates) > 0:
+            raise ValueError(f"'--submission_renaming_keys' ({args.submission_renaming_keys}) leads to the following "
+                             f"duplicate renaming values:\n{duplicates}\nMust choose different renaming keys")
+else:
+    info_df = None
 
 # If the number of the exercise is specified, use it. Otherwise, try to extract/infer it from the submission filename.
 exercise_num = args.number if args.number is not None else \
     extract_exercise_number(args.submissions_file, args.exercise_names)
 
-tutors = pd.read_csv(args.tutors_file, header=None) if args.tutors_file is not None else \
+tutors_df = pd.read_csv(args.tutors_file, header=None) if args.tutors_file is not None else \
     extract_weighted_tutors(args.tutors_list)
-assert len(tutors.columns) == 1 or len(tutors.columns) == 2
+assert len(tutors_df.columns) == 1 or len(tutors_df.columns) == 2
 # Assign equal default weights if only tutor names were specified to ensure we have a weight column.
-if len(tutors.columns) == 1:
-    tutors[1] = 1
-tutors.columns = ["name", "weight"]
-tutors["name"] = np.roll(tutors["name"], exercise_num)
-tutors["weight"] = np.roll(tutors["weight"], exercise_num)
+if len(tutors_df.columns) == 1:
+    tutors_df[1] = 1
+tutors_df.columns = ["name", "weight"]
+tutors_df["name"] = np.roll(tutors_df["name"], exercise_num)
+tutors_df["weight"] = np.roll(tutors_df["weight"], exercise_num)
 # Handle duplicate tutor names by simply adding increasing numbers after the name.
-handle_duplicate_names(tutors)
+handle_duplicate_names(tutors_df)
 
 unzip_dir = args.submissions_file + "_UNZIPPED"
 print(f"extracting submissions ZIP file to '{get_file_path(unzip_dir, args.print_abs_paths)}'")
 with zipfile.ZipFile(args.submissions_file, "r") as f:
     f.extractall(unzip_dir)
-submission_dirs = sorted(os.listdir(unzip_dir))
+submissions_df = get_submissions_df(os.listdir(unzip_dir), FULL_NAME_COL, MOODLE_ID_COL, SUBMISSION_COL)
+if args.sorting_keys:
+    first_name_col = args.info_file_first_name_key
+    last_name_col = args.info_file_last_name_key
+    if first_name_col is None:
+        first_name_col, last_name_col = match_full_names(submissions_df[FULL_NAME_COL], info_df)
+        print(f"identified '{first_name_col}' as first name column and '{last_name_col}' as last name column")
+    info_df[FULL_NAME_COL] = info_df[first_name_col] + " " + info_df[last_name_col]
+    merged_df = pd.merge(submissions_df, info_df, on=FULL_NAME_COL, how="inner")
+    assert len(submissions_df) == len(merged_df)
+    submissions_df = merged_df.sort_values(by=args.sorting_keys)
+else:
+    submissions_df.sort_values(SUBMISSION_COL, inplace=True)
 
-print(f"distributing {len(submission_dirs)} submissions among the following {len(tutors)} tutors:")
-print(tutors)
-for i, chunk in enumerate(weighted_chunks(submission_dirs, tutors["weight"])):
-    chunk_file = f"{args.submissions_file[:-4]}_{tutors['name'][i]}.zip"
+if args.submission_renaming_keys:
+    name_format = args.submission_renaming_separator.join(f"<{k}>" for k in args.submission_renaming_keys)
+    print(f"Renaming submissions according to the following format: {name_format}")
+
+print(f"distributing {len(submissions_df)} submissions among the following {len(tutors_df)} tutors:")
+print(tutors_df)
+for i, chunk_df in enumerate(weighted_chunks(submissions_df, tutors_df["weight"])):
+    chunk_file = f"{args.submissions_file[:-4]}_{tutors_df['name'][i]}.zip"
     with zipfile.ZipFile(chunk_file, "w") as f:
-        # Write all files from the submission directory to the tutors ZIP file. Must exclude directories,
-        # since glob includes them. Also specify the relative path as name in the ZIP file (arcname), as
-        # otherwise, the full absolute path would be stored in the ZIP file.
-        for submission_dir in chunk:
-            for file in glob(os.path.join(unzip_dir, submission_dir, "**"), recursive=True):
+        # Write all files from the submission directory to the tutors ZIP file. Must exclude directories, since glob
+        # includes them. Also specify the relative path as name in the ZIP file (arcname), as otherwise, the full
+        # absolute path would be stored in the ZIP file.
+        for _, entry in chunk_df.iterrows():
+            for file in glob(os.path.join(unzip_dir, entry[SUBMISSION_COL], "**"), recursive=True):
                 if os.path.isfile(file):
-                    f.write(file, arcname=file[len(unzip_dir) + 1:])
-    print(f"[{i + 1}/{len(tutors)}] {len(chunk):3d} submissions ---> {get_file_path(chunk_file, args.print_abs_paths)}")
+                    if args.submission_renaming_keys:
+                        name = args.submission_renaming_separator.join(entry[k] for k in args.submission_renaming_keys)
+                        name = os.path.join(name, os.path.basename(file))
+                    else:
+                        name = file[len(unzip_dir) + 1:]
+                    f.write(file, arcname=name)
+    print(f"[{i + 1}/{len(tutors_df)}] {len(chunk_df):3d} submissions ---> "
+          f"{get_file_path(chunk_file, args.print_abs_paths)}")
 
 print(f"deleting extracted submissions directory '{get_file_path(unzip_dir, args.print_abs_paths)}'")
 shutil.rmtree(unzip_dir, ignore_errors=True)
